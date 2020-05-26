573ac441092f7411210d0e8d6c27a795a21d45dd
Gass, Paul CTR <paul.gass@isso.net>
2020-05-25 21:41:53 -0400

    json test3

--- assets/js/mdjson.js
+++ assets/js/mdjson.js
@@ -1,5 +1,5 @@
 $().ready(function(){
-    $.getJSON( "../nkatgass.json", function( data ) {
+    $.getJSON( "../js/nkatgass.json", function( data ) {
     console.log(data);
     $("#text").html(data["text"]);
   });
